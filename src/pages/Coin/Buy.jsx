2348cbe339e70014a4f33b89a7bb581285b1813d
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-06-11 23:07:39 +0530

    buy changes

--- src/pages/Coin/Buy.jsx
+++ src/pages/Coin/Buy.jsx
@@ -42,8 +42,8 @@ function Buy({ handleBuyPopup, buyPopup, coinName, coinSymbol, cp, coinImage }) 
                     <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-transparent text-black duration-200 p-4 rounded shadow-md w-[300px] border-solid border-2 border-white'>
 
                         <div className='flex items-center justify-between'>
-                            <h1 className='font-bold mx-14  text-white '> Buy <span className='bg-violet-700 text-white rounded-full p-2 '>{coinName}</span></h1>
-                            <div className='text-2xl cursor-pointer' onClick={handleBuyPopup}>
+                            <h1 className='font-bold mx-10  text-white '> Buy <span className='bg-violet-700 text-white rounded-full p-2 '>{coinName}</span></h1>
+                            <div className='text-2xl cursor-pointer text-white' onClick={handleBuyPopup}>
                                 <i class="fa-solid fa-xmark"></i>
                             </div>
 
@@ -58,19 +58,20 @@ function Buy({ handleBuyPopup, buyPopup, coinName, coinSymbol, cp, coinImage }) 
                                     <input className='text-gray-700 border appearance-none block text-lg p-1 mt-2' type='number' value={count} onChange={handleCount} />
                                 </div>
                             </div>
+
                             <div className='w-full px-3'>
-                                <label className='font-bold text-black bg-white tracking-wide mb-2 text-lg rounded-full p-1'>
+                                <label className='font-bold text-black bg-violet-700 tracking-wide mb-2 text-lg rounded-full p-1'>
                                     Crypto Currency
                                 </label>
                                 <h2 className='text-white font-bold pt-2 text-lg flex gap-2'>{coinName}-{coinSymbol.toUpperCase()} <img src={coinImage} className='w-9' /></h2>
                             </div>
 
-                            <div className='w-full md:w-1/2 px-3 mt-2'>
-                                <label className='font-bold text-black bg-white tracking-wide mb-2 text-lg rounded-full p-1 mt-2'>Currency</label>
-                                <select className='px-3 text-black mt-2 ml-2' onChange={currencyHandler}>
-                                    <option value='usd'>USD</option>
-                                    <option value='eur'>EUR</option>
-                                    <option value='inr'>INR</option>
+                            <div className='w-full md:w-1/2 px-3 mt-2 flex gap-2'>
+                                <label className='font-bold text-black bg-white tracking-wide mb-2 text-lg rounded-full p-1 mt-2'>Currency:</label>
+                                <select className='bg-transparent text-white mt-2 border-solid border-2 border-white rounded-md py-0 px-2' onChange={currencyHandler}>
+                                    <option value='usd' className='bg-blue-950 text-white'>USD</option>
+                                    <option value='eur' className='bg-blue-950 text-white'>EUR</option>
+                                    <option value='inr' className='bg-blue-950 text-white'>INR</option>
                                 </select>
                             </div>
 
